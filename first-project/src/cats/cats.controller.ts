8ad2062806532686ef79a8309c7282058a529ce6
jiwonpg98 <jiwonpg98@gmail.com>
2022-01-02 23:48:54 +0900

    Modify: logger와 pipe 실습 추가

--- first-project/src/cats/cats.controller.ts
+++ first-project/src/cats/cats.controller.ts
@@ -2,25 +2,31 @@ import {
   Controller,
   Delete,
   Get,
+  HttpException,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
   Put,
+  UseFilters,
 } from "@nestjs/common";
+import { HttpExceptionFilter } from "src/http-exception.filter";
 import { CatsService } from "./cats.service";
 
 @Controller("cats")
+@UseFilters(HttpExceptionFilter)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Get()
   getAllCats() {
-    return "all cats";
+    throw new HttpException("api is broken", 401);
   }
 
   @Get(":id")
-  getOneCat(@Param() param) {
-    return `${param.id} is here`;
+  getOneCat(@Param("id", ParseIntPipe) param) {
+    console.log(typeof param);
+    return `${param} is here`;
   }
 
   @Post()

7ba70286e398832ccd3969104c7cd53b4901e152
jiwonpg98 <jiwonpg98@gmail.com>
2022-01-03 00:31:28 +0900

    Modify: 폴더 정리 및 interceptors 추가

--- first-project/src/cats/cats.controller.ts
+++ first-project/src/cats/cats.controller.ts
@@ -9,11 +9,15 @@ import {
   Post,
   Put,
   UseFilters,
+  UseInterceptors,
 } from "@nestjs/common";
-import { HttpExceptionFilter } from "src/http-exception.filter";
+import { HttpExceptionFilter } from "src/common/exceptions/http-exception.filter";
+import { SuccessInterceptor } from "src/common/interceptors/success.interceptor";
+import { PositiveIntPipe } from "src/common/pipes/positiveInt.pipe";
 import { CatsService } from "./cats.service";
 
 @Controller("cats")
+@UseInterceptors(SuccessInterceptor)
 @UseFilters(HttpExceptionFilter)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
@@ -24,7 +28,7 @@ export class CatsController {
   }
 
   @Get(":id")
-  getOneCat(@Param("id", ParseIntPipe) param) {
+  getOneCat(@Param("id", ParseIntPipe, PositiveIntPipe) param: number) {
     console.log(typeof param);
     return `${param} is here`;
   }
---
+++ first-project/src/common/pipes/positiveInt.pipe.ts
@@ -0,0 +1,8 @@
+import { Injectable, PipeTransform } from "@nestjs/common";
+
+@Injectable()
+export class PositiveIntPipe implements PipeTransform {
+  transform(value: number) {
+    return value;
+  }
+}

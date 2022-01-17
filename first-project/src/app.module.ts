b6eaeec1d26ad116bfc0f2c70b667640e8e1808a
jiwonpg98 <jiwonpg98@gmail.com>
2022-01-17 17:38:50 +0900

    Refactor: 폴더 정리

--- first-project/src/app.module.ts
+++ first-project/src/app.module.ts
@@ -1,11 +1,22 @@
 import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { CatsModule } from "./cats/cats.module";
 import { LoggerMiddleware } from "./common/middlewares/logger.middleware";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: true,
+    }),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

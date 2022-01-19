3ba5e5299460eaae3d34700beee0f5391faf6a0e
jiwonpg98 <jiwonpg98@gmail.com>
2022-01-20 00:10:54 +0900

    Modify: swagger 기본 설정 및 cors 설정

--- first-project/src/main.ts
+++ first-project/src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 import { HttpExceptionFilter } from "./common/exceptions/http-exception.filter";
 
@@ -7,6 +8,20 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe()); // 스키마에서 클래스 validation을 쓰려면 이걸 등록해야됌!!!
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.enableCors({
+    origin: true, // 개발용 (배포할 때는 특정 url)
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle("C.I.C")
+    .setDescription("Cats api")
+    .setVersion("1.0.0")
+    .addTag("cats")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, document);
+
   const PORT = process.env.PORT;
   await app.listen(PORT);
 }

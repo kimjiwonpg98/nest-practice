70f269917c10da06543346189fb5bd5d62737bf3
jiwonpg98 <jiwonpg98@gmail.com>
2022-01-19 22:27:48 +0900

    Modify: validation을 쓰기 위한 설정 추가

--- first-project/src/main.ts
+++ first-project/src/main.ts
@@ -1,9 +1,11 @@
+import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { HttpExceptionFilter } from "./common/exceptions/http-exception.filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe()); // 스키마에서 클래스 validation을 쓰려면 이걸 등록해야됌!!!
   app.useGlobalFilters(new HttpExceptionFilter());
   const PORT = process.env.PORT;
   await app.listen(PORT);
